2de03dfb7e5c15cff3158084870ffe0fa0ff7d18
Josip Bošnjak <jbosnjak3@gmail.com>
2026-03-11 13:09:12 +0100

    update
    maknut datum iščlanjenja

--- Videoteka/resources/views/clanska_iskaznica/edit.blade.php
+++ Videoteka/resources/views/clanska_iskaznica/edit.blade.php
@@ -46,13 +46,9 @@
          @enderror 
 
 
-        <label for="datum_isclanjenja" class="block text-gray-300 mb-2 text-sm font-medium">Datum iščlanjenja</label>
-         <input type="date" name="datum_isclanjenja" id="datum_isclanjenja" value="{{ old('datum_isclanjenja',$clanovi->datum_isclanjenja?->format("Y-m-d")) }}" class="w-full px-4 py-2 bg-gray-700 text-white rounded-lg border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition duration-200">
-         @error('datum_isclanjenja')
-            <p class="text-red-600 text-sm">{{ $message }}</p>
-         @enderror 
+       
     
-        <button type="submit" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 rounded-lg transition duration-200 shadow-lg">Ažuriraj ćlana videoteke</button>
+        <button type="submit" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 rounded-lg transition duration-200 shadow-lg">Ažuriraj člana videoteke</button>
       </form>
     </div>
    
